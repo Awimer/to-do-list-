79f436898be28e23b01d39e2a7ebc078ac0c689a
ibrahim <ebrahim.awimai@gmail.com>
2022-12-07 21:19:38 +0200

    add design of home connect the project to fire base

---
+++ lib/my_theme.dart
@@ -0,0 +1,37 @@
+
+
+import 'package:flutter/material.dart';
+
+class MyTheme{
+  static const Color lightPrimary = Color(0xFF5D9CEC);
+  static const Color lightScaffoldBackground = Color(0XFFDFECDB);
+  static const Color green = Color(0xFF61E757);
+  static const Color red = Color(0xFFEC4B4B);
+  static final lightTheme = ThemeData(
+    primaryColor: lightPrimary,
+    scaffoldBackgroundColor: lightScaffoldBackground,
+    bottomNavigationBarTheme: BottomNavigationBarThemeData(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      selectedIconTheme: IconThemeData(
+        size: 36,
+        color: lightPrimary,
+      ),
+      unselectedIconTheme: IconThemeData(
+        size: 36,
+      )
+    ),
+      textTheme: TextTheme(
+          titleMedium: TextStyle(
+            color: lightPrimary,
+            fontSize: 24,
+        fontWeight:FontWeight.bold,
+      ),
+
+        bodySmall: TextStyle(
+        fontSize: 12,
+        color: Colors.black,
+      )
+    )
+  );
+}
